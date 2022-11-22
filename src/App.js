2a881e571a79c9018dcb9c11e7dd83af6a2e11c6
HBKLXNDR <hbklksndr@protonmail.com>
2022-11-23 00:50:00 +0200

    redux_toolkit_cons

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
-import './App.css';
+import {useForm} from "react-hook-form";
+import axios from "axios";
+import {useEffect, useState} from "react";
 
 function App() {
+    const {register, handleSubmit} = useForm({defaultValues: {id: 1}});
+    const [users, setUsers] = useState([])
+    const [user, setUser] = useState(null)
+
+    useEffect(() => {
+        axios.get("https://jsonplaceholder.typicode.com/users").then(({data}) => setUsers(data))
+    }, [])
+
+    const submit = async ({id}) => {
+       const {data} = await axios.get(`https://jsonplaceholder.typicode.com/users/${id}`);
+       setUser(data)
+    }
+
     return (
         <div>
+            {/*<form onChange={handleSubmit(submit)}>   in this case you should take off btn and it will change*/}
+            <form onSubmit={handleSubmit(submit)}>
+                <select {...register("id")}>
+                    {users.map(user => (
+                        <option key={user.id} value={user.id}>{user.name}</option>
+                    ))}
+                </select>
+                <button>getInfo</button>
+            </form>
+            {user && <div>{user.id}.)  {user.name}. Email: {user.email}</div>}
         </div>
+
     );
 }
 
